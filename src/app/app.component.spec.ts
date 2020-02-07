71728db7b539ab27735b74363ab31a71e64ac9de
Shubhram Krishna <shubhram.krishna@gmail.com>
2020-02-07 16:34:08 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the app', () => { const fixture = TestBed.createComponent(AppComponent);const app = fixture.debugElement.componentInstance;expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ngx-code-prettify'`, () => {
